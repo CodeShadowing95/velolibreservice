ea0c7aee92bcffc5966a6959ff8865c10098544d
VastoLorde <ksenbonsakura@gmail.com>
2024-05-02 11:44:39 +0200

    Some changes added

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -96,7 +96,7 @@ const InteractiveMap = () => {
         {Object.keys(stationFromContract).length !== 0 ?
           <Marker longitude={stationFromContract.position.longitude} latitude={stationFromContract.position.latitude} style={{ zIndex: 1 }} pitchAlignment="viewport">
             <div className="flex relative justify-center cursor-pointer hover:scale-125 transition-transform">
-              <img src="/public/map-marker.png" alt="map marker" className="w-8 h-8 animate-bounce" />
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 animate-bounce" viewBox="0 0 24 24"><path fill="#7e22ce" fillRule="evenodd" d="M12 2c-4.418 0-8 4.003-8 8.5c0 4.462 2.553 9.312 6.537 11.174a3.45 3.45 0 0 0 2.926 0C17.447 19.812 20 14.962 20 10.5C20 6.003 16.418 2 12 2m0 10a2 2 0 1 0 0-4a2 2 0 0 0 0 4" clipRule="evenodd"/></svg>
             </div>
           </Marker>
           :
@@ -105,8 +105,7 @@ const InteractiveMap = () => {
             <Marker longitude={station.position.longitude} latitude={station.position.latitude} key={station.name} style={{ zIndex: 1 }} pitchAlignment="viewport" onClick={() => handleLocationChange(station.position.latitude, station.position.longitude, station.number)}>
               <div key={station.number} className="flex relative justify-center cursor-pointer hover:scale-125 transition-transform">
                 {stationLocation.latitude === station?.position.latitude && stationLocation.longitude === station?.position.longitude ?
-                  // <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 animate-bounce" viewBox="0 0 24 24"><path fill="#7e22ce" fillRule="evenodd" d="M12 2c-4.418 0-8 4.003-8 8.5c0 4.462 2.553 9.312 6.537 11.174a3.45 3.45 0 0 0 2.926 0C17.447 19.812 20 14.962 20 10.5C20 6.003 16.418 2 12 2m0 10a2 2 0 1 0 0-4a2 2 0 0 0 0 4" clipRule="evenodd"/></svg>
-                  <img src="/public/map-marker.png" alt="map marker" className="w-8 h-8 animate-bounce" />
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 animate-bounce" viewBox="0 0 24 24"><path fill="#7e22ce" fillRule="evenodd" d="M12 2c-4.418 0-8 4.003-8 8.5c0 4.462 2.553 9.312 6.537 11.174a3.45 3.45 0 0 0 2.926 0C17.447 19.812 20 14.962 20 10.5C20 6.003 16.418 2 12 2m0 10a2 2 0 1 0 0-4a2 2 0 0 0 0 4" clipRule="evenodd"/></svg>
                 :
                   <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8" viewBox="0 0 24 24"><path fill="#0ea5e9" d="M12 2c-4.418 0-8 4.003-8 8.5c0 4.462 2.553 9.312 6.537 11.174a3.45 3.45 0 0 0 2.926 0C17.447 19.812 20 14.962 20 10.5C20 6.003 16.418 2 12 2" opacity=".5"/><path fill="#ffffff" d="M12 12.5a2.5 2.5 0 1 0 0-5a2.5 2.5 0 0 0 0 5"/></svg>
                 }
